f6e664bab710732e8f6afa49a51db83b47085f8b
Margarita Morozova <ritamorozova@yahoo.com>
2020-08-17 18:44:24 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] == " " || board[index] == ""
     false 
-  elsif board[index] == "X" && board[index] == "O"
+  elsif board[index] == "X" || board[index] == "O"
     true
   end 
 end 
